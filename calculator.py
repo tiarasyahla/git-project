19064554535081c652ffb46e01d045946405c3c5
tiarasyahla <tiarasyahlameitira@student.upi.edu>
2025-10-29 15:59:44 +0700

    add perkalian dan pembagian

--- calculator.py
+++ calculator.py
@@ -3,6 +3,10 @@ b = 5
 
 tambah = a + b
 kurang = b - a
+kali = a * b
+bagi = b / a
 
 print("Hasil tambah:", tambah)
 print("Hasil kurang:", kurang)
+print("Hasil kali:", kali)
+print("Hasil bagi:", bagi)
