b40e67d0b6ee2212dee61520f7bcfe22364f2367
tiarasyahla <tiarasyahlameitira@student.upi.edu>
2025-11-05 15:16:55 +0700

    edit calculator.py dan add file bebas.py

--- calculator.py
+++ calculator.py
@@ -5,8 +5,12 @@ tambah = a + b
 kurang = b - a
 kali = a * b
 bagi = b / a
+kuadrat_a = a ** 2
+kuadrat_b = b ** 2
 
 print("Hasil tambah:", tambah)
 print("Hasil kurang:", kurang)
 print("Hasil kali:", kali)
 print("Hasil bagi:", bagi)
+print("Kuadrat a:", kuadrat_a)
+print("Kuadrat b:", kuadrat_b)
